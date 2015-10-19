fcb1ac711f899e3c0a3110f324d4d99f05ef7ff6
Donna Robinson <donna@moviemeetinghouse.com>
2015-10-19 17:21:12 +0000

    Added Composer, Node and Bower Dependencies With Config

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,15 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+$api = app('Dingo\Api\Routing\Router');
+$dispatcher = app('Dingo\Api\Dispatcher');
+
+// Dingo generated router for Version 1 of the app API
+$api->version('v1', function($api){
+
+    // Set the namespace for the API_v1-specific routes
+    $api->group(['namespace' => 'App\Api\Controllers', 'middleware' => 'cors'], function($api){
+
+        // Routes that do NOT require authentication
+
+        // Routes that DO require (JWT) authentication
+        $api->group(['middleware' => 'token.auth'], function($api){
+            //
+        });
+
+    });
+
+});
 
 Route::get('/', function () {
     return view('welcome');
--- config/app.php
+++ config/app.php
@@ -13,8 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
-
+    'debug'           => env('APP_DEBUG', false),
     /*
     |--------------------------------------------------------------------------
     | Application URL
@@ -26,8 +25,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
-
+    'url'             => 'http://localhost',
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
@@ -39,8 +37,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
-
+    'timezone'        => 'UTC',
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
@@ -52,8 +49,7 @@ return [
     |
     */
 
-    'locale' => 'en',
-
+    'locale'          => 'en',
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
@@ -66,7 +62,6 @@ return [
     */
 
     'fallback_locale' => 'en',
-
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
@@ -78,10 +73,8 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
-
-    'cipher' => 'AES-256-CBC',
-
+    'key'             => env('APP_KEY', 'SomeRandomString'),
+    'cipher'          => 'AES-256-CBC',
     /*
     |--------------------------------------------------------------------------
     | Logging Configuration
@@ -95,8 +88,7 @@ return [
     |
     */
 
-    'log' => 'single',
-
+    'log'             => 'single',
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
@@ -108,7 +100,7 @@ return [
     |
     */
 
-    'providers' => [
+    'providers'       => [
 
         /*
          * Laravel Framework Service Providers...
@@ -140,13 +132,16 @@ return [
         /*
          * Application Service Providers...
          */
+        Dingo\Api\Provider\LaravelServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Class Aliases
@@ -158,41 +153,45 @@ return [
     |
     */
 
-    'aliases' => [
+    'aliases'         => [
+        'App'        => Illuminate\Support\Facades\App::class,
+        'Artisan'    => Illuminate\Support\Facades\Artisan::class,
+        'Auth'       => Illuminate\Support\Facades\Auth::class,
+        'Blade'      => Illuminate\Support\Facades\Blade::class,
+        'Bus'        => Illuminate\Support\Facades\Bus::class,
+        'Cache'      => Illuminate\Support\Facades\Cache::class,
+        'Config'     => Illuminate\Support\Facades\Config::class,
+        'Cookie'     => Illuminate\Support\Facades\Cookie::class,
+        'Crypt'      => Illuminate\Support\Facades\Crypt::class,
+        'DB'         => Illuminate\Support\Facades\DB::class,
+        'Eloquent'   => Illuminate\Database\Eloquent\Model::class,
+        'Event'      => Illuminate\Support\Facades\Event::class,
+        'File'       => Illuminate\Support\Facades\File::class,
+        'Gate'       => Illuminate\Support\Facades\Gate::class,
+        'Hash'       => Illuminate\Support\Facades\Hash::class,
+        'Input'      => Illuminate\Support\Facades\Input::class,
+        'Inspiring'  => Illuminate\Foundation\Inspiring::class,
+        'Lang'       => Illuminate\Support\Facades\Lang::class,
+        'Log'        => Illuminate\Support\Facades\Log::class,
+        'Mail'       => Illuminate\Support\Facades\Mail::class,
+        'Password'   => Illuminate\Support\Facades\Password::class,
+        'Queue'      => Illuminate\Support\Facades\Queue::class,
+        'Redirect'   => Illuminate\Support\Facades\Redirect::class,
+        'Redis'      => Illuminate\Support\Facades\Redis::class,
+        'Request'    => Illuminate\Support\Facades\Request::class,
+        'Response'   => Illuminate\Support\Facades\Response::class,
+        'Route'      => Illuminate\Support\Facades\Route::class,
+        'Schema'     => Illuminate\Support\Facades\Schema::class,
+        'Session'    => Illuminate\Support\Facades\Session::class,
+        'Storage'    => Illuminate\Support\Facades\Storage::class,
+        'URL'        => Illuminate\Support\Facades\URL::class,
+        'Validator'  => Illuminate\Support\Facades\Validator::class,
+        'View'       => Illuminate\Support\Facades\View::class,
 
-        'App'       => Illuminate\Support\Facades\App::class,
-        'Artisan'   => Illuminate\Support\Facades\Artisan::class,
-        'Auth'      => Illuminate\Support\Facades\Auth::class,
-        'Blade'     => Illuminate\Support\Facades\Blade::class,
-        'Bus'       => Illuminate\Support\Facades\Bus::class,
-        'Cache'     => Illuminate\Support\Facades\Cache::class,
-        'Config'    => Illuminate\Support\Facades\Config::class,
-        'Cookie'    => Illuminate\Support\Facades\Cookie::class,
-        'Crypt'     => Illuminate\Support\Facades\Crypt::class,
-        'DB'        => Illuminate\Support\Facades\DB::class,
-        'Eloquent'  => Illuminate\Database\Eloquent\Model::class,
-        'Event'     => Illuminate\Support\Facades\Event::class,
-        'File'      => Illuminate\Support\Facades\File::class,
-        'Gate'      => Illuminate\Support\Facades\Gate::class,
-        'Hash'      => Illuminate\Support\Facades\Hash::class,
-        'Input'     => Illuminate\Support\Facades\Input::class,
-        'Inspiring' => Illuminate\Foundation\Inspiring::class,
-        'Lang'      => Illuminate\Support\Facades\Lang::class,
-        'Log'       => Illuminate\Support\Facades\Log::class,
-        'Mail'      => Illuminate\Support\Facades\Mail::class,
-        'Password'  => Illuminate\Support\Facades\Password::class,
-        'Queue'     => Illuminate\Support\Facades\Queue::class,
-        'Redirect'  => Illuminate\Support\Facades\Redirect::class,
-        'Redis'     => Illuminate\Support\Facades\Redis::class,
-        'Request'   => Illuminate\Support\Facades\Request::class,
-        'Response'  => Illuminate\Support\Facades\Response::class,
-        'Route'     => Illuminate\Support\Facades\Route::class,
-        'Schema'    => Illuminate\Support\Facades\Schema::class,
-        'Session'   => Illuminate\Support\Facades\Session::class,
-        'Storage'   => Illuminate\Support\Facades\Storage::class,
-        'URL'       => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View'      => Illuminate\Support\Facades\View::class,
+        'JWTAuth'    => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        'APIRoute'   => Dingo\Api\Facade\Route::class,
+        'API'        => Dingo\Api\Facade\API::class,
 
     ],
 
